98d130920d5c98336d5111a11bff83449e6d777d
llopez9047 <llopez9047@utm.edu.ec>
2026-08-06 22:21:54 -0500

    modificar arranque y agregar appmodule

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NavegacionComponent } from './components/navegacion.component';
+import { DashboardComponent } from './components/dashboard.component';
+import { RegistroIncidenteComponent } from './components/registro-incidente.component';
+import { ListadoTicketsComponent } from './components/listado-tickets.component';
+import { TicketService } from './ticket.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavegacionComponent,
+    DashboardComponent,
+    RegistroIncidenteComponent,
+    ListadoTicketsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [TicketService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient() // Habilita la comunicación con tu backend en Render
-  ]
-}).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
