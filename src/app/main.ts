d9fd7d70984cef3229685af00d138377b4aa09e2
llopez9047 <llopez9047@utm.edu.ec>
2026-08-06 22:18:30 -0500

    borrar innecesarios

--- src/app/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient() // Habilita la comunicación con tu backend en Render
-  ]
-}).catch(err => console.error(err));
